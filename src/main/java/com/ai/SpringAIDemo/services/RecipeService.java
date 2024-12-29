de00bf61676a68effa9496d0ce51874a59abcc4a
Luca Broggiato <lucabroggy@gmail.com>
2024-12-29 17:42:01 +0100

    Fixed prompt template

--- src/main/java/com/ai/SpringAIDemo/services/RecipeService.java
+++ src/main/java/com/ai/SpringAIDemo/services/RecipeService.java
@@ -10,8 +10,9 @@ import java.util.Map;
 @Service
 public class RecipeService {
     private final String CREATE_RECIPE_TEMPLATE = "I want to create a recipe with the following ingredients: {ingredients}." +
-            "The cuisine type I prefer is: {cuisine}." +
-            "Consider and apply the following dietary restrictions: {restrictions}" +
+            "Consider that you are not obligated to use all of these ingredients, but it is mandatory that you do not use any external ingredient." +
+            "Also, the cuisine type I prefer is: {cuisine}." +
+            "Consider and apply the following dietary restrictions, which are mandatory: {restrictions}" +
             "Please, provide me with a detailed recipe including Title, List of Ingredients and Cooking Instructions.";
 
     private final ChatModel chatModel;
