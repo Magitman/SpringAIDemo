6924bc2920ff06d65d7a69db096d115656fa4a0c
Luca Broggiato <l.broggiato@reply.it>
2024-12-29 12:06:18 +0100

    Implemented call using options

--- src/main/java/com/ai/SpringAIDemo/services/ChatService.java
+++ src/main/java/com/ai/SpringAIDemo/services/ChatService.java
@@ -1,7 +1,12 @@
 package com.ai.SpringAIDemo.services;
 
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ChatService {
     private final ChatModel chatModel;
 
@@ -11,5 +16,18 @@ public class ChatService {
 
     public String getResponse(String prompt) {
         return this.chatModel.call(prompt);
+    }
+
+    public String getResponseOptions(String prompt) {
+        ChatResponse chatResponse = this.chatModel.call(
+                new Prompt(
+                        prompt,
+                        OpenAiChatOptions.builder()
+                                .withModel("gpt-4o")
+                                .withTemperature(0.4)
+                                .build()
+                ));
+
+        return chatResponse.getResult().getOutput().getContent();
     }
 }
