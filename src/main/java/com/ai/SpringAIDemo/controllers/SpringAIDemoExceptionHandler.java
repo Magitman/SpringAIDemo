48256ec3700d88be0406cb18956928cc3e7833e3
Magitman <lucabroggy@gmail.com>
2025-01-01 12:26:04 +0100

    Created global exception handler

---
+++ src/main/java/com/ai/SpringAIDemo/controllers/SpringAIDemoExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.ai.SpringAIDemo.controllers;
+
+import com.ai.SpringAIDemo.exceptions.RecipeNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class SpringAIDemoExceptionHandler {
+
+    @ExceptionHandler(RecipeNotFoundException.class)
+    public ResponseEntity<?> handleRecipeNotFoundException(RecipeNotFoundException ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGlobalException(Exception ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
